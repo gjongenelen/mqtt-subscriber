fa1f3d49738d66c85c0e934ff815a6d6e8452bf3
Gino Jongenelen <g.jongenelen@pushto.space>
2016-06-23 16:10:03 +0200

    JWT option

--- start.py
+++ start.py
@@ -5,6 +5,7 @@ import time
 import http.client
 import json
 import datetime
+import urllib.parse
 import paho.mqtt.client as mqtt
 from threading import Thread
 
@@ -30,7 +31,7 @@ class HttpRequest(Thread):
 
 class Process(Thread):
     def __init__(self, sid=None, host=None, username=None, password=None, topic=None, topic_qos=None,
-                 destination_type=None, destination=None):
+                 destination_type=None, destination=None, auth_type=None, auth_server=None, auth_endpoint=None):
         Thread.__init__(self)
         self.sid = sid
         self.host = host
@@ -40,10 +41,35 @@ class Process(Thread):
         self.topic_qos = topic_qos
         self.destination_type = destination_type
         self.destination = destination
+        self.auth_type = auth_type
+        self.auth_server = auth_server
+        self.auth_endpoint = auth_endpoint
         self.client = None
+
+    def get_jwt(self, auth_server, username, password):
+        connection = http.client.HTTPSConnection(self.auth_server)
+        connection.request('POST', "/" + self.auth_endpoint, urllib.parse.urlencode({'email': self.username, 'password': self.password }), {"Content-type": "application/x-www-form-urlencoded" })
+        response = connection.getresponse()
+        data = json.loads(response.read().decode())
+        connection.close()
+        return data['token']
 
     def on_connect(self, client, obj, flags, rc):
         self.client.subscribe(self.topic, qos=self.topic_qos)
+
+    def on_disconnect(self, client, userdata, rc):
+        print(rc)
+        time.sleep(5)
+        if rc != 0:
+            if self.auth_type == "jwt":
+                self.set_auth()
+            self.connect()
+
+    def set_auth(self):
+        if self.auth_type == "jwt":
+            self.client.username_pw_set(self.get_jwt(self.auth_server, self.username, self.password), "token")
+        else:
+            self.client.username_pw_set(self.username, self.password)
 
     def on_message(self, client, obj, msg):
         try:
@@ -55,13 +81,17 @@ class Process(Thread):
         except:
             pass
 
+    def connect(self):
+        self.client.connect(self.host, 1883, 60)
+        self.client.loop_start()
+
     def run(self):
         self.client = mqtt.Client(str(self.sid) + "_subscriber")
         self.client.on_connect = self.on_connect
+        self.client.on_disconnect = self.on_disconnect
         self.client.on_message = self.on_message
-        self.client.username_pw_set(self.username, self.password)
-        self.client.connect(self.host, 1883, 60)
-        self.client.loop_forever()
+        self.set_auth()
+        self.connect()
 
 
 def main(argv):
@@ -79,7 +109,7 @@ def main(argv):
             for subscription in result:
                 process = Process(subscription['id'], argv[4], argv[5], argv[6], subscription['topic'],
                                   subscription['topic_qos'], subscription['target_type'],
-                                  subscription['target'])
+                                  subscription['target'],  argv[7], argv[8], argv[9])
                 process.start()
                 processes.append(process)
     finally:
